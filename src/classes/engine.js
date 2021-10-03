4b85837c16c8fb1482fb7b32cc40a6a19dfbce57
Ava <ava239@yandex.ru>
2021-10-03 23:31:31 +0300

    add token to terms processing to search phrase

--- src/classes/engine.js
+++ src/classes/engine.js
@@ -1,20 +1,25 @@
+import _ from 'lodash';
+
 class SearchEngine {
   constructor(docs = []) {
     this.docs = docs;
   }
 
-  process() {
-    this.docs = this.docs.map((doc) => {
-      const tokens = doc.text.split(' ');
-      const terms = tokens.reduce((acc, token) => [...acc, ...token.match(/\w+/g)], []);
-      return { ...doc, tokens, terms };
-    });
+  textProcess(text) {
+    const tokens = text.split(' ');
+    const terms = tokens.reduce((acc, token) => [...acc, ...token.match(/\w+/g) ?? []], []);
+    return { tokens, terms };
+  }
+
+  process(docs) {
+    return docs.map((doc) => ({ ...doc, ...this.textProcess(doc.text) }));
   }
 
   search(needle) {
-    this.process();
-    return this.docs
-      .filter(({ terms }) => terms.includes(needle))
+    const docs = this.process(this.docs);
+    const { terms: searchTerms } = this.textProcess(needle);
+    return docs
+      .filter(({ terms }) => _.intersection(terms, searchTerms).length === searchTerms.length)
       .map(({ id }) => id);
   }
 }
