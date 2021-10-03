c47da9dafa8c0d6d3529fa0705229aed28a8699c
Ava <ava239@yandex.ru>
2021-10-03 23:39:39 +0300

    no classes

--- src/engine.js
+++ src/engine.js
@@ -1,7 +1,22 @@
 // @ts-check
+import _ from 'lodash';
 
-import SearchEngine from './classes/engine';
+const processText = (text) => {
+  const tokens = text.split(' ');
+  const terms = tokens.reduce((acc, token) => [...acc, ...token.match(/\w+/g) ?? []], []);
+  return { tokens, terms };
+};
 
-const buildSearchEngine = (docs) => new SearchEngine(docs);
+const processCollection = (docs) => docs.map((doc) => ({ ...doc, ...processText(doc.text) }));
+
+const buildSearchEngine = (docs) => ({
+  search: (needle) => {
+    const processedDocs = processCollection(docs);
+    const { terms: searchTerms } = processText(needle);
+    return processedDocs
+      .filter(({ terms }) => _.intersection(terms, searchTerms).length === searchTerms.length)
+      .map(({ id }) => id);
+  },
+});
 
 export default buildSearchEngine;
